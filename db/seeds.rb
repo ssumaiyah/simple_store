2ea7da12cf33d26050735cbfce97e9ba12f94c2a
ssumaiyah <ssumaiyah@academic.rrc.ca>
2024-06-17 10:24:43 -0500

    updated seeds.rb

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,9 +1,22 @@
-CSV.foreach(Rails.root.join('db', 'products.csv'), headers: true) do |row|
-  category = Category.find_or_create_by(name: row['category'])
-  category.products.create(
-    title: row['title'],
-    description: row['description'],
-    price: row['price'],
-    stock_quantity: row['stock_quantity']
-  )
+require 'csv'
+
+Product.destroy_all
+Category.destroy_all
+
+csv_file = Rails.root.join('db/products.csv')
+csv_data = File.read(csv_file)
+
+products = CSV.parse(csv_data, headers: true)
+
+products.each do |product|
+  category_name = product['category']
+  category = Category.find_or_create_by(name: category_name)
+
+  Product.create(
+    title: product['name'],
+    description: product['description'],
+    price: product['price'],
+    stock_quantity: product['stock quantity'],
+    category: category
+)
 end
