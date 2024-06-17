1c463a26f7ec6e5f10a79ee54ecf36ff5dd98573
ssumaiyah <ssumaiyah@academic.rrc.ca>
2024-06-17 09:40:34 -0500

    Updated db/seeds.rb to pull data from products.csv

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,11 +1,9 @@
-require "csv"
-require 'faker'
-
-676.times do
-  Product.create(
-    title: Faker::Commerce.product_name,
-    description: Faker::Lorem.paragraph,
-    price: Faker::Commerce.price,
-    stock_quantity: Faker::Number.number(digits: 3)
+CSV.foreach(Rails.root.join('db', 'products.csv'), headers: true) do |row|
+  category = Category.find_or_create_by(name: row['category'])
+  category.products.create(
+    title: row['title'],
+    description: row['description'],
+    price: row['price'],
+    stock_quantity: row['stock_quantity']
   )
 end
