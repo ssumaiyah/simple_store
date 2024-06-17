408b927b485a7ddc404ccc05fc959c499f0e4699
ssumaiyah <ssumaiyah@academic.rrc.ca>
2024-06-17 09:33:21 -0500

    Generated Category model

--- app/models/product.rb
+++ app/models/product.rb
@@ -1,6 +1,3 @@
 class Product < ApplicationRecord
-    validates :title, presence: true
-    validates :price, presence: true
-    validates :stock_quantity, presence: true
-  end
-  
+  belongs_to :category
+end
